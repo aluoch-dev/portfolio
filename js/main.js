fdfabea0a28e822f0b50b746eb7ada3eef568bc2
Laurine Adhiambo <laurinealuoch7@gmail.com>
2021-04-20 08:43:23 +0300

    smoothscroll js function

--- js/main.js
+++ js/main.js
@@ -7,3 +7,32 @@ function navigationIcon() {
         x.className = "top-navigation";
     }
     }
+
+//Add smoothscroll to links
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The number (1200) specifies the number of milliseconds it takes to scroll to 
+        // the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1200, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+    
